8df4c541dd2f94f08c54695978220d3ddd3e9fde
prataffel <24615963+prataffel@users.noreply.github.com>
2021-04-07 18:15:42 +0200

    Draft of a fixer that splits preferred at punctuation
    ToDo:
     * Bug with multiple identical lines
     * Indentation is not preserved

--- src/index.js
+++ src/index.js
@@ -16,33 +16,71 @@ String.prototype.bytes = function () {
     }
     return length;
 };
-module.exports = function (context, options = defaultOptions) {
+
+
+function split_text(string, length){
+    var splitted_text = ""
+    var rest = string;
+    // list of characters that are searched for linebreaks.
+    const breaking_symbols = [": ", ". ", "! ", "? "];
+    while (rest.bytes() > length) {
+        var line = rest.substring(0, length);
+        var breaking_points = breaking_symbols.map(x => line.lastIndexOf(x));
+        var split_pos = Math.max(...breaking_points);
+        // If no symbol was found, split at the last space
+        if (split_pos == -1) {
+            split_pos = line.lastIndexOf(" ") -1
+        }
+        splitted_text += line.substring(0, split_pos + 1)
+        splitted_text += "\n"
+        
+        rest = rest.substring(split_pos + 2, rest.bytes())
+    }
+    splitted_text += rest;
+    return splitted_text;
+}
+
+const reporter = (context, options = defaultOptions) => {
     options = ObjectAssign({}, defaultOptions, options);
-    const max = options.max_width;
     const {
         Syntax,
         RuleError,
+        fixer,
         report,
         getSource
     } = context;
+    const max = options.max_width;
     return {
         [Syntax.Document](node) {
-            const text = getSource(node).split("\n");
+            const text = getSource(node);
+            const splitted = text.split("\n");
             for (const key in text) {
-                // console.log(key)
-                if (text.hasOwnProperty(key)) {
-                    const elem = text[key];
+                if (splitted.hasOwnProperty(key)) {
+                    const elem = splitted[key];
                     const len = elem.bytes();
-                    // console.log(elem)
-                    // console.log(len)
+
                     if (len > max) {
-                        report(node, new RuleError(`Line is too long(now width: ${len}).`, {
-                            line: parseInt(key),
-                            column: max
-                        }));
+                        var index = text.search(elem)
+                        var replacementstring = split_text(elem, max)
+                        const replace = fixer.replaceTextRange([index, index + len], replacementstring);
+                        report(
+                            node, new RuleError(
+                                `Line is too long(now width: ${len}).`, {
+                                    line: parseInt(key),
+                                    column: max,
+                                    fix: replace,
+                                }
+                            )
+                        );
                     }
                 }
             }
         }
     }
+};
+
+module.exports = {
+    linter: reporter,
+    // This rule has fixer.
+    fixer: reporter
 };
